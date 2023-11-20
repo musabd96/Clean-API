545c7687232dc54309429da45e3a5f72310d971d
musabd96 <mustafa.abdulle@student.nbi-handelsakademin.se>
2023-11-20 15:53:54 +0100

    Application

---
+++ Application/Queries/Dogs/GetDogById/GetDogByIdQueryHabdler.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Modulea;
+using Infrastructure.Database;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.Dogs.GetDogById
+{
+    internal class GetDogByIdQueryHabdler : IRequestHandler<GetDogByIdQuery, Dog>
+    {
+        private readonly MockDatabase _mockDatabase;
+
+        public GetDogByIdQueryHabdler(MockDatabase mockDatabase)
+        {
+            _mockDatabase = mockDatabase;
+        }
+
+        public Task<Dog> Handle(GetDogByIdQuery request, CancellationToken cancellationToken)
+        {
+            Dog wantedDag = _mockDatabase.allDogs.Where(Dog => Dog.animalId == request.Id).FirstOrDefault()!;
+            return Task.FromResult(wantedDag);
+        }
+    }
+}
---
+++ Application/Queries/Dogs/UpdateDogById/UpdateDogByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Modulea;
+using Infrastructure.Database;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.Dogs.UpdateDogById
+{
+    internal class UpdateDogByIdQueryHandler : IRequestHandler<UpdateDogByIdQuery, Dog>
+    {
+        private readonly MockDatabase _mockDatabase;
+
+        public UpdateDogByIdQueryHandler(MockDatabase mockDatabase)
+        {
+            _mockDatabase = mockDatabase;
+        }
+
+        public Task<Dog> Handle(UpdateDogByIdQuery request, CancellationToken cancellationToken)
+        {
+            Dog existingDog = _mockDatabase.allDogs.Where(Dog => Dog.animalId == request.Id).FirstOrDefault()!;
+            if (existingDog == null)
+            {
+                throw new NotFiniteNumberException($"Dog with ID {request.Id} not found.");
+            }
+
+            existingDog.Name = request.Name;
+
+
+            return Task.FromResult(existingDog);
+        }
+    }
+}
